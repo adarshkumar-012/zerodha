484b4606e8a5c809f75052000a70acb6eb66ad40
adarshkumar-012 <adarshguta012@gmail.com>
2026-09-08 12:49:04 +0530

    Fix build errors (alt tags and unused vars)

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 function Signup() {
-
-  const navigate = useNavigate();
 
   const [user, setUser] = useState({
     username: "",
